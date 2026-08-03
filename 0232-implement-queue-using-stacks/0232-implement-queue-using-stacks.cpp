dda3188d2d59c1a7b1bd39b243c49def5876c136
Rohan Kanaujia <rohankanaujia90@gmail.com>
2026-08-03 12:54:39 +0530

    Time: 0 ms (100%), Space: 10.1 MB (48.36%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,48 @@
+#include <stack>
+using namespace std;
+
+class MyQueue {
+    stack<int> s1, s2;
+
+public:
+    MyQueue() {
+    }
+
+    void push(int x) {
+        s1.push(x);
+    }
+
+    int pop() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        if (s2.empty())
+            return -1;
+
+        int val = s2.top();
+        s2.pop();
+        return val;
+    }
+
+    int peek() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        if (s2.empty())
+            return -1;
+
+        return s2.top();
+    }
+
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
